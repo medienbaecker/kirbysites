a9585a7959ae25b5ddf6720d953767510623c0f5
Thomas Günther <mail@medienbaecker.com>
2020-03-03 14:05:42 +0100

    Content update

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,7 +2,7 @@
     <a class="logo" href="<?= $site->url() ?>">
         <?= $site->title() ?>
     </a>
-    <a href="mailto:submit@kirbysites.com" class="submit">
+    <a href="mailto:submit@kirbysites.com?subject=Submit%20to%20Kirbysites&body=Please%20attach%20a%20few%20panel%20screenshots." class="submit">
         <?= svg('assets/images/add.svg')?>
         Submit
     </a>
